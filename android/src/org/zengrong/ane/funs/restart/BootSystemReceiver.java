b5b914431e194f818004220d807167d98cfe05e3
Rect <revtvv@gmail.com>
2013-05-21 23:41:20 +0800

    更新restart  加入严谨的服务开启判断
    解决部分机器会因为关闭未开启的服务而出现异常的问题

--- android/src/org/zengrong/ane/funs/restart/BootSystemReceiver.java
+++ android/src/org/zengrong/ane/funs/restart/BootSystemReceiver.java
@@ -1,5 +1,9 @@
 package org.zengrong.ane.funs.restart;
 
+import java.util.ArrayList;
+
+import android.app.ActivityManager;
+import android.app.ActivityManager.RunningServiceInfo;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
@@ -13,6 +17,7 @@ public class BootSystemReceiver extends BroadcastReceiver
 {
 
 	public static final String TAG = "org.zengrong.ane.funs.restart.BootSystemReceiver";
+	public static final String severName = "org.zengrong.ane.funs.restart.NotificationService";
 	@Override
 	public void onReceive(Context context, Intent intent)
 	{
@@ -27,7 +32,17 @@ public class BootSystemReceiver extends BroadcastReceiver
 		else 
 		{
 			Log.d(TAG, "停止  服务");
-			context.stopService(serviceIntent);
+			ActivityManager myManager=(ActivityManager)context.getSystemService(Context.ACTIVITY_SERVICE);
+			ArrayList<RunningServiceInfo> runningService = (ArrayList<RunningServiceInfo>) myManager.getRunningServices(30);
+			for(int i = 0 ; i<runningService.size();i++)
+			{
+				if(runningService.get(i).service.getClassName().toString().equals(severName))
+				{
+					Log.d(TAG, "执行停止");
+					context.stopService(serviceIntent);
+					break;
+				}
+			}
 		}
 	}
 }
